1532790b13bd8adf757a751428f2059ed6df4807
糖炒栗子 <Yang_chaoqi@outlook.com>
2026-03-25 13:39:33 +0800

    inheritHeight

--- AI_vue/src/components/BackendLayout.vue
+++ AI_vue/src/components/BackendLayout.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="backend-layout">
     <el-container class="main-container">
-      <Sidebar />
-      <el-container direction="vertical">
-        <Navbar />
+      <Sidebar/>
+      <el-container>
+        <Navbar/>
         <el-main>
-          <router-view></router-view>
+            <router-view></router-view>
         </el-main>
       </el-container>
     </el-container>
@@ -15,14 +15,15 @@
 <script setup>
 import Navbar from './Navbar.vue';
 import Sidebar from './Sidebar.vue'
+
 </script>
 
 <style lang="scss" scoped>
 .backend-layout {
   height: 100vh;
-}
-
-.main-container {
-  height: 100%;
+  // 继承父元素的高度
+  .main-container {
+    height: 100%;
+  }
 }
 </style>
--- AI_vue/src/components/Sidebar.vue
+++ AI_vue/src/components/Sidebar.vue
@@ -3,11 +3,17 @@
   <el-aside width="264px">
     <el-menu
         default-active="2"
-        class="el-menu-vertical-demo"
         @open="handleOpen"
         @close="handleClose"
+        class="menu-style"
       >
-        
+        <div class="brand">
+            <el-image style="width: 50px; height: 50px;margin-right: 10px;" :src="iconUrl" alt="logo"></el-image>
+            <div class="info-card">
+                <h1 class="brand-title">心理健康AI助手</h1>
+                <p class="brand-subtitle">管理后台</p>
+            </div>
+        </div>
         <el-menu-item v-for="item in router.options.routes[0].children" :key="item.path" :index="item.path">
             <!-- 动态组件component传入图标，实现组件渲染 -->
           <el-icon><component :is="item.meta.icon" /></el-icon>
@@ -21,6 +27,8 @@
 <script setup>
 import {useRouter} from 'vue-router'
 
+const iconUrl = new URL('/src/assets/images/机器人.png', import.meta.url).href
+
 const router=useRouter()
 
 console.log(router,'router')
@@ -33,3 +41,31 @@ const handleClose=(key)=>{
     console.log(key)
 }
 </script>
+
+<style lang="scss" scoped>
+.menu-style {
+    
+    height: 100%;
+    .brand {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+    background-color: #f5f7fa;
+    border: 1px solid #e4e7ed;
+    .info-card {
+        .brand-title {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 5px;
+            color: #303133;
+        }
+        .brand-subtitle {
+            font-size: 14px;
+            color: #606266;
+        }
+    }
+}
+}
+
+</style>
