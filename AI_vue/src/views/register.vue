cfa0c2b8ff68e51a0fec834e1504ec15784c9507
糖炒栗子 <Yang_chaoqi@outlook.com>
2026-04-02 00:35:41 +0800

    registerForm

--- AI_vue/src/views/register.vue
+++ AI_vue/src/views/register.vue
@@ -1,3 +1,97 @@
 <template>
-    <div>注册页</div>
+    <div class="container">
+        <div class="title">
+            <div class="title-text">
+                <h2>创建您的账户</h2>
+                <p>请填写注册信息</p>
+            </div>
+        </div>
+        <el-form class="form-container">
+            <el-form label-position="top" :model="formData" :rules="rules" ref="submitFormRef">
+                <el-form-item label="用户名或邮箱" prop="username">
+                    <el-input v-model="formData.username" placeholder="请输入用户名或邮箱"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="formData.email" placeholder="请输入邮箱"></el-input>
+                </el-form-item>
+                <el-form-item label="昵称" prop="nickname">
+                    <el-input v-model="formData.nickname" placeholder="请输入昵称（可选）"></el-input>
+                </el-form-item>
+                <el-form-item label="手机号" prop="phone">
+                    <el-input v-model="formData.phone" placeholder="请输入手机号（可选）"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input v-model="formData.password" placeholder="请输入密码" type="password"></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="confirmPassword">
+                    <el-input v-model="formData.confirmPassword" placeholder="请再次输入密码" type="password"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button class="btn" type="primary" size="large" @click="submitForm(submitFormRef)">注册</el-button>
+                </el-form-item>
+            </el-form>
+        </el-form>
+    </div>
 </template>
+<script setup>
+import { ref, reactive ,onMounted } from 'vue'
+
+const formData = ref({
+    "username": "",
+    "email": "",
+    "nickname": "",
+    "phone": "",
+    "password": "",
+    "confirmPassword": "",
+    "gender": 0,
+    "userType": 1//普通用户
+})
+
+const rules = reactive({//表单验证规则:必需属性
+    username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+    email: [{ required: true, message: '请输入邮箱', trigger: 'blur' }],
+    password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+    confirmPassword: [{ required: true, message: '请输入确认密码', trigger: 'blur' }],
+})
+// 提交表单
+const submitFormRef = ref(null)
+const submitForm = async (formEl) => {
+    if(!formEl) return
+    formEl.validate(async (valid) => {
+        
+    })
+}
+</script>
+<style lang="scss" scoped>
+.container {
+    width: 384px;
+    .flex-box {
+        display: flex;
+        align-items: center;
+    }
+    .title {
+        .title-text {
+            text-align: center;
+            h2 {
+                font-size: 36px;
+                margin-bottom: 10px;
+            }
+            p {
+                font-size: 18px;
+                color: #6b7280;
+            }
+        }
+    }
+    .form-container {
+        margin-top: 30px;
+        .btn {
+            margin-top: 40px;
+            width: 100%;
+        }
+        .footer {
+            padding: 30px;
+            text-align: center;
+        }
+    }
+}
+</style>
